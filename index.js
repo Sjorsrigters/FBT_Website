1268b6268c31d1fca360a54c71c8171b419df0ec
Sjors Rigters <55089936+Sjorsrigters@users.noreply.github.com>
2021-03-23 11:28:12 +0100

    Test

---
+++ index.js
@@ -0,0 +1,30 @@
+$('.showSingle').click(function () {
+    if ($(this).hasClass("button-active")) {
+      $('.targetDiv').hide();
+      $('.showSingle').removeClass('button-active');
+      $('#div1, #div5').show();
+    } else {
+      $('.targetDiv').hide();
+      $('.showSingle').removeClass('button-active');
+      $(this).addClass("button-active")
+      $('#div' + $(this).attr('target')).show();
+    }
+});
+
+$(document).ready(function() {
+			var colors = ["#aa5aff", "#fc5217"];
+			var rand = Math.floor(Math.random() * colors.length);
+			$('body').css("background-color", colors[rand]);
+});
+
+// $(document).ready(function() {
+// 			var colors = ["#aa5aff", "grey"];
+// 			var rand = Math.floor(Math.random() * colors.length);
+// 			$('header').css("background-color", colors[rand]);
+//       $('footer').css("background-color", colors[rand]);
+// });
+
+var scale = Math.min(
+  availableWidth / contentWidth,
+  availableHeight / contentHeight
+);
